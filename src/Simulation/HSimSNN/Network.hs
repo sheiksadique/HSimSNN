105cbab65a5298098ae249d61651f6ffa964c32b
Sadique Sheik <sheiksadique@gmail.com>
2015-07-07 05:03:32 -0700

    added above threshold function

--- src/Simulation/HSimSNN/Network.hs
+++ src/Simulation/HSimSNN/Network.hs
@@ -1,7 +1,9 @@
+-- | Network module includes functions and data types that are involved in simulating the network
 module Simulation.HSimSNN.Network where
 
 import Simulation.HSimSNN.Population
 import Simulation.HSimSNN.Connections
 
+-- | Network data type encapsulates a network of neurons by holding a Population and its Connections
 data Network = Network {population:: Population, connections:: Connections}
                deriving Show
--- src/Simulation/HSimSNN/Neuron.hs
+++ src/Simulation/HSimSNN/Neuron.hs
@@ -1,4 +1,12 @@
+
+-- | Neuron module encapsulates behavior of a Neuron
 module Simulation.HSimSNN.Neuron where
 
 data Neuron = Neuron {vmem:: Double}
               deriving Show
+
+-- | Checks if the membrane potential of a neuron is above threshold value
+aboveThreshold:: Neuron -> Double -> Bool
+aboveThreshold neuron threshold
+        | threshold > vmem neuron = False
+        | otherwise = True
