ded4d7b99897e26fb6ddd21204e626d892682c16
Sadique Sheik <sheiksadique@gmail.com>
2015-07-08 05:42:05 -0700

    added some documentation on the structure of Connections data structure

--- src/Simulation/HSimSNN/Connections.hs
+++ src/Simulation/HSimSNN/Connections.hs
@@ -8,5 +8,10 @@ import Simulation.HSimSNN.Neuron
 
 -- | Connections encapsulates information about source and destination neurons
 -- and corresponding synaptic information
-data Connections = Connections {srcPop:: Population, destPop:: Population, syninfo::[[SynInfo]]}
+--
+-- * If the source population is of size N and destination population of size M, syninfo will be a list of size N, where each element in the list is a list of all destination neurons, 
+-- * The index of the top level list of syninfo corresponds to the index of neurons in srcPop - (0,(N-1))
+-- * The first int in the tuple is the index of neuron in the destPop - (0..(M-1))
+
+data Connections = Connections {srcPop:: Population, destPop:: Population, syninfo::[[(Int,SynInfo)]]}
                    deriving Show
