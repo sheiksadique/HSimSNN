84994421efc198912877ed8a95670877aef98b02
Sadique Sheik <sheiksadique@gmail.com>
2015-07-07 18:26:58 -0700

    added missing module import file

---
+++ src/Simulation/HSimSNN.hs
@@ -0,0 +1,11 @@
+module Simulation.HSimSNN 
+    (
+        module Simulation.HSimSNN.Connections,
+        module Simulation.HSimSNN.Network,
+        module Simulation.HSimSNN.Neuron,
+        module Simulation.HSimSNN.Population,
+    ) where
+import Simulation.HSimSNN.Connections
+import Simulation.HSimSNN.Network
+import Simulation.HSimSNN.Neuron
+import Simulation.HSimSNN.Population
