2ac715a741f5ac73bc5b3abe121b946d074b4174
Sadique Sheik <sheiksadique@gmail.com>
2015-07-21 12:04:43 -0700

    basic applySpike function implemented

--- src/Simulation/HSimSNN/Neuron.hs
+++ src/Simulation/HSimSNN/Neuron.hs
@@ -24,13 +24,13 @@ threshold = 1.0
 
 -- | Neuron is defined by its state and time at which its state was last evaluated
 -- The state of a neuron is defined as list of doubles
-data Neuron = Neuron {state::V.Vector Double, tlastupdate::Double, tnextspike::SPK.NextSpikeTime}
+data Neuron = Neuron {state::V.Vector Double, tlastupdate::Double}
 -- | String representation for Neuron
 instance Show Neuron where
-    show (Neuron st tl _) = "Neuron (" ++ (show $(V.toList) st) ++ " @ " ++ (show tl) ++ ")"
+    show (Neuron st tl) = "Neuron (" ++ (show $(V.toList) st) ++ " @ " ++ (show tl) ++ ")"
 
 -- | Initializes a neuron with a given state at time 0
-initNeuron st = Neuron (V.fromList st) 0 SPK.Never -- TODO: Hardcoding Never incorrect
+initNeuron st = Neuron (V.fromList st) 0 -- TODO: Hardcoding Never incorrect
 
 -- | Returns the membrane potential of a neuron
 vmem:: Neuron -> Double
@@ -45,7 +45,7 @@ vmem neuron = (V.head.state) neuron -- For now the state of a neuron is the firs
 evaluateNeuronStateAtt:: Neuron -> Double -> Neuron
 evaluateNeuronStateAtt neuron t 
                 |t == (tlastupdate neuron) = neuron -- The neuron has already been updated
-                |t > (tlastupdate neuron) = Neuron newstate t SPK.Never -- TODO: Hardcoding Never .. incorrect
+                |t > (tlastupdate neuron) = Neuron newstate t
                 |otherwise = error "This neuron has already been updated to the future"
                 where
                     decayfact = exp ((tlastupdate neuron)-t) -- decay factor
@@ -68,10 +68,18 @@ aboveThreshold neuron
 resetNeuron:: Neuron -> Double -> Neuron
 resetNeuron neuron t 
                 |tlastupdate neuron > t = error "Neuron has already been updated to the future"
-                |otherwise = Neuron newstate t SPK.Never
+                |otherwise = Neuron newstate t
                 where
                     newstate = V.map (*0) $ state neuron -- neuron dynamics
                   
+
+-- | Apply a presynaptic spike to a neuron
+applySpike :: SynInfo -> Double -> Neuron -> Neuron
+applySpike (SynInfo w typ) spktm neuron = Neuron newstate spktm
+    where
+        Neuron curstate _ = evaluateNeuronStateAtt neuron spktm
+        newstate = V.fromList [(V.head) newstate + w] V.++ ((V.tail) newstate)
+
 
 -- | Evaluate the next possible spike time of a neuron given its state at time t
 -- 
@@ -81,4 +89,4 @@ resetNeuron neuron t
 nextSpikeTime:: Neuron -> SPK.NextSpikeTime
 nextSpikeTime neuron
     |aboveThreshold neuron = SPK.At $ tlastupdate neuron
-    |otherwise =  SPK.At $(threshold-vmem neuron) + tlastupdate neuron --SPK.Never
+    |otherwise =  SPK.At $(threshold-vmem neuron) + tlastupdate neuron 
