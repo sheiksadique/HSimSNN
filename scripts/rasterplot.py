eecfa99da4551f00111fdc713aec9664936e364c
Sadique Sheik <sheiksadique@gmail.com>
2015-12-11 23:33:27 -0800

    added script to visualize data

---
+++ scripts/rasterplot.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pylab
+
+def loadHData(fname='../exampleinput.txt'):
+    data = []
+    with open(fname) as f:
+        zs = f.readlines()
+    for z in zs:
+        data.append([x.strip('[()]') for x in z.split(',')])
+    
+    data = np.array(data)
+    data.flatten()
+    data = data.astype('float').reshape((-1,2))
+    return data
+
+def plotHData(fname='../exampleinput.txt'):
+    data = loadHData(fname)
+    pylab.plot(data[:,1], data[:,0], '.')
+
+
+
+if __name__=='__main__':
+    plotHData(fname='exampleinput.txt')
+    plotHData(fname='exampleoutput.txt')
+    pylab.legend(['Input','Output'])
+    pylab.show()
+
