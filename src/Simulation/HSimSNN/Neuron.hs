80a60d0ae0c0c56bfe28431420f878e0bf25eb1e
Tristan Webb <tristan@fpcomplete.com>
2016-01-06 21:43:52 -0800

    Spaces after guards

--- src/Simulation/HSimSNN/Neuron.hs
+++ src/Simulation/HSimSNN/Neuron.hs
@@ -62,12 +62,12 @@ aboveThreshold neuron = vmem neuron > threshold
 -- Hardcasting threshold to 1.0 TODO: should parametrize somehow
 -- See above use reader monad for parameters
 resetNeuron:: Neuron -> Double -> Neuron
-resetNeuron neuron t 
-                |tLastUpdate neuron > t = error $
-                                            (show t) 
-                                            ++ "Neuron has already been updated to the future" 
-                                            ++ (show $tLastUpdate neuron) -- for debugging
-                |otherwise = Neuron newstate t
+resetNeuron neuron t
+                | tLastUpdate neuron > t = error $
+                                             (show t)
+                                             ++ "Neuron has already been updated to the future"
+                                             ++ (show $tLastUpdate neuron) -- for debugging
+                | otherwise = Neuron newstate t
                 where
                     -- Rewrite this without lists
                     y = V.tail $ state neuron -- time of last spike
@@ -75,27 +75,27 @@ resetNeuron neuron t
                     newstate = [0,t] V.++ y
 
 -- | Evaluate the next possible spike time of a neuron given its state at time t
--- 
+--
 -- This function is essentially what defines the dynamics of the neuron. (not really.. it depends on the dynamics though)
 -- Currently the neuron receives a constant input current
 -- Ideally this should be something users can define and pass at the top level
 nextSpikeTime:: Neuron -> SPK.NextSpikeTime
 nextSpikeTime neuron
-    |aboveThreshold neuron = SPK.At $ tLastUpdate neuron
-    |otherwise = SPK.Never
-    -- -- |otherwise =  SPK.At $(threshold-vmem neuron) + tlastupdate neuron 
+    | aboveThreshold neuron = SPK.At $ tLastUpdate neuron
+    | otherwise = SPK.Never
+    -- -- |otherwise =  SPK.At $(threshold-vmem neuron) + tlastupdate neuron
 
 
 -- | Evaluate state of neuron at time t
 -- Ideally used at the arrival of a spike or when the neuron spikes (when an
 -- event occoured)
 evaluateNeuronStateAtt:: Neuron -> Double -> Neuron
-evaluateNeuronStateAtt neuron t 
-                |t > (tLastUpdate neuron) = Neuron newstate t
-                |t == (tLastUpdate neuron) = neuron -- The neuron has already been updated
-                |otherwise = error $ (show t) 
-                                     ++ "Neuron has already been updated to the future" 
-                                     ++ (show $tLastUpdate neuron) -- for debugging
+evaluateNeuronStateAtt neuron t
+                | t > (tLastUpdate neuron) = Neuron newstate t
+                | t == (tLastUpdate neuron) = neuron -- The neuron has already been updated
+                | otherwise = error $ (show t)
+                                      ++ "Neuron has already been updated to the future"
+                                      ++ (show $tLastUpdate neuron) -- for debugging
                 where
                     taum = 10.0
                     decayfact = exp (((tLastUpdate neuron)-t)/taum) -- decay factor
@@ -109,8 +109,8 @@ evaluateNeuronStateAtt neuron t
 -- | Apply a presynaptic spike to a neuron at time t
 applySynapticSpikeToNeuron :: SynInfo -> Double -> Neuron -> Neuron
 applySynapticSpikeToNeuron (SynInfo w _) spktm neuron
-                |isRefractoryAtt neuron spktm = Neuron curstate spktm
-                |otherwise = Neuron newstate spktm
+                | isRefractoryAtt neuron spktm = Neuron curstate spktm
+                | otherwise = Neuron newstate spktm
                 where
                     Neuron curstate _ = evaluateNeuronStateAtt neuron spktm
                     newstate = V.modify (\v -> VM.modify v (+w) 0) curstate
@@ -118,8 +118,8 @@ applySynapticSpikeToNeuron (SynInfo w _) spktm neuron
 -- | Check if a neuron is still refractory
 isRefractoryAtt:: Neuron -> Double -> Bool
 isRefractoryAtt (Neuron oldstate tlastupdate) t
-                |(t-tlastupdate) > tref  = False -- Neuron has not been modified within refractory time window
-                |(t-(oldstate V.! 1)) > tref = False -- last spike time was before refractory time window
-                |otherwise = True
+                | (t-tlastupdate) > tref  = False -- Neuron has not been modified within refractory time window
+                | (t-(oldstate V.! 1)) > tref = False -- last spike time was before refractory time window
+                | otherwise = True
                 where
                     tref = 0.5
